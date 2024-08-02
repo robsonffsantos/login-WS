ad147c5616cef5663e735e9013443bafd5723f02
Robson <robson.2501@hotmail.com>
2024-08-02 02:40:26 -0300

    feat: add i18, configure translation json

---
+++ src/i18.ts
@@ -0,0 +1,27 @@
+// src/i18n.ts
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import enTranslation from './locales/en/translation.json';
+import ptTranslation from './locales/pt/translation.json';
+
+// Configuração do i18next
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      pt: {
+        translation: ptTranslation,
+      },
+    },
+    fallbackLng: 'pt', // Idioma padrão
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
